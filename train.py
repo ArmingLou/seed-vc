49cbacdb38aeffc8c27ee5d29902db47197a5fdb
Arming <arming.lou@foxmail.com>
2025-12-23 21:40:24 +0800

    修改

--- train.py
+++ train.py
@@ -1611,7 +1611,7 @@ class Trainer:
 
         # find all checkpoints and remove old ones
         checkpoints = glob.glob(os.path.join(self.log_dir, 'DiT_epoch_*.pth'))
-        if len(checkpoints) > 3:
+        if len(checkpoints) > 4:
             checkpoints.sort(key=lambda x: int(x.split('_')[-1].split('.')[0]))
             for cp in checkpoints[:-2]:
                 os.remove(cp)
